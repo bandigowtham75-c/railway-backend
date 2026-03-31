a636c7475fff345083ab364053ed0a23df5edd98
bandigowtham75-c <bandigowtham75@gmail.com>
2026-03-31 23:39:15 +0530

    final

--- src/main/java/com/example/railway/config/CorsConfig.java
+++ src/main/java/com/example/railway/config/CorsConfig.java
@@ -2,28 +2,21 @@ package com.example.railway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
-@CrossOrigin(origins = "https://railway-monitoring-system-frontend.onrender.com")
-@RestController
-@RequestMapping("/api/tracks")
-public class TrackController {
-}
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://railway-monitoring-system-frontend.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
